62de763043ba0ba5dccad0de1ee3ab862c8b2983
SAI MOHAN <45478292+MohanCloud-stack@users.noreply.github.com>
2023-02-22 09:21:55 +0530

    added the 2.sh file with new changes

--- 2.sh
+++ 2.sh
@@ -6,12 +6,8 @@ REGION="us-east-1"
 IMAGE_NAME="hub.docker.com/devopsmohan/kaiburnodejs:39"
 
 # Create the EKS cluster
-#eksctl create cluster --name $CLUSTER_NAME --region $REGION
-eksctl create cluster --name  $CLUSTER_NAME --version 1.21 --region $REGION --zones=us-east-1a,us-east-1b
-#eksctl create cluster --name kaiburnode --region us-east-1 --zones=us-east-1a,us-east-1b
-
-
-
+#ieksctl create cluster --name $CLUSTER_NAME --region $REGION
+eksctl create cluster --name  $CLUSTER_NAME --version 1.21  --region $REGION --zones=us-east-1a,us-east-1b
 # Deploy the Docker image to the cluster
-kubectl run $IMAGE_NAME --image=myregistry.com/myimage:v1 --port=80
+kubectl run $IMAGE_NAME --image=hub.docker.com/devopsmohan/kaiburnodejs:39 --port=80
 kubectl expose deployment $IMAGE_NAME --type=LoadBalancer --port=80 --target-port=80
