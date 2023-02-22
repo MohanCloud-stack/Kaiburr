5c7a92e4d21b5a8363831347dfc0352ec4bdd354
SAI MOHAN <45478292+MohanCloud-stack@users.noreply.github.com>
2023-02-22 07:15:16 +0530

    added the 2.sh file

---
+++ 2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define variables
+CLUSTER_NAME="kaiburnode"
+REGION="us-east-1"
+IMAGE_NAME="hub.docker.com/devopsmohan/kaiburnodejs:39"
+
+# Create the EKS cluster
+eksctl create cluster --name $CLUSTER_NAME --region $REGION
+
+# Deploy the Docker image to the cluster
+kubectl run $IMAGE_NAME --image=myregistry.com/myimage:v1 --port=80
+kubectl expose deployment $IMAGE_NAME --type=LoadBalancer --port=80 --target-port=80
